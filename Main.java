835cd85757c2dc41dc9c8bb515913431719b17bc
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-05-13 11:39:45 +0200

    personnage sauvegardé dans une variable

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 package com.heroQuest;
 import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String inputSpecialRule = sc.nextLine();
         Menu menu = new Menu();
-        menu.chooseYourCharacter();
-        SpecialRules leave = new SpecialRules();
-        leave.leaveCavern();
+        CommunPeople aventurier = menu.chooseYourCharacter();
+        if (inputSpecialRule.equals("arcane sacrés")) {
+            menu.chooseSpecialRules();
+        }
+
     }
 }
