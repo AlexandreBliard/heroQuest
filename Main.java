a32019a37a573d56cf3cac936063390ac5292114
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-05-13 14:33:32 +0200

    Main resolved

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 package com.heroQuest;
 import java.awt.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        Scanner sc = new Scanner(System.in);
-        String inputSpecialRule = sc.nextLine();
         Menu menu = new Menu();
-        CommunPeople aventuirer = menu.chooseYourCharacter();
-        if (inputSpecialRule.equals("arcane sacrés")) {
-            menu.chooseSpecialRules();
-        }
-
+        menu.chooseYourCharacter();
+        SpecialRules leave = new SpecialRules();
+        leave.leaveCavern();
     }
 }
