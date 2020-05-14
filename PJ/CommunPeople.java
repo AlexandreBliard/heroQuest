9a8e41c83cdad2fc45b49fccc570eb2ed40daa83
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-05-14 14:24:04 +0200

    debut refactor constructor des classe filles

--- PJ/CommunPeople.java
+++ PJ/CommunPeople.java
@@ -5,21 +5,22 @@ import com.heroQuest.stuff.Weapons;
 public class CommunPeople {
 
     protected Weapons weapons = new Weapons();
-
+/*---ATTRIBUTS------ATTRIBUTS------ATTRIBUTS------*/
     protected String name;
     protected int life;
     protected Weapons attack = weapons;
 
-    public Weapons setWeapons (Weapons Cweapons) {
-        this.weapons = Cweapons;
-        return Cweapons;
-    }
 
     /*---------les SETTERS sont ici ------------*/
     public void setName(String name) { this.name = name; }
     public void setLife(int life) {this.life = life;}
     public void setAttack(Weapons attack) {this.attack = attack;}
-
+    /*-------SETTER de WEAPONS indispensable pour
+    appliquer les armes dans la partie--------------*/
+    public Weapons setWeapons (Weapons Cweapons) {
+        this.weapons = Cweapons;
+        return Cweapons;
+    }
  /*--------les GETTERS sont ici------------*/
     public String getName() {return name;}
     public int getLife() {return life;}
