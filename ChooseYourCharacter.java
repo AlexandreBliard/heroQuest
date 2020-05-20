b6202f47e7ad75c8107b14b63e5605c5ea245dff
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-05-20 10:49:45 +0200

    suppression des lignes de tests

--- ChooseYourCharacter.java
+++ ChooseYourCharacter.java
@@ -18,7 +18,6 @@ public class ChooseYourCharacter {
                 + " es-tu un vaillant guerrier " +
                 "ou un rusé magicien ?");
         CommunPeople aventurier = null;
-        CommunPeople ennemy = null;
         boolean enigme = false;
         do {
             System.out.println("dites au sage si " +
@@ -27,9 +26,7 @@ public class ChooseYourCharacter {
             String classePJ = keyboard.nextLine();
             if (classePJ.equals("GUERRIER") || classePJ.equals("MAGICIEN")) {
                 aventurier = createAventurer(classePJ, namePJ);
-                ennemy = new Goblins();
                 System.out.println(aventurier);
-                System.out.println(ennemy);
                 enigme = true;
                 return aventurier;
             } else if (classePJ.equals("guerrier") || classePJ.equals("magicien")) {
