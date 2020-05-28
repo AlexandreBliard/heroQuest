9d60b98a0dc3596a15fc19ffe0e8d1ca1bb6f87f
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-05-28 13:46:23 +0200

    création de la méthode maxLife en interface

---
+++ pj/MaxLife.java
@@ -0,0 +1,10 @@
+package com.heroquest.pj;
+
+public interface MaxLife {
+
+     static void maxLife(int level, int life) {
+         int  maxLife =  level * life;
+    }
+
+
+}
--- pj/Warrior.java
+++ pj/Warrior.java
@@ -3,7 +3,7 @@ package com.heroquest.pj;
 import com.heroquest.stuff.SmallSword;
 import com.heroquest.stuff.Loot;
 
-public class Warrior extends CommunPeople {
+public class Warrior extends CommunPeople implements MaxLife{
 
     public Warrior(String type, String name, int life, Loot loot) {
         super(type, name, life, loot);
@@ -14,7 +14,9 @@ public class Warrior extends CommunPeople {
     public Warrior(String name) {
         this(name, new SmallSword());
     }
-/*à l'occase il serait pratique de voir si interface permet de gérer
+
+
+    /*à l'occase il serait pratique de voir si interface permet de gérer
 * le stuff du PJ*/
 }
 /*TD : reprendre pour pouvoir mettre
